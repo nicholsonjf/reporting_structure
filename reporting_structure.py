eda798ce806a9a5219973b167ff9a313e5aae670
nicholsonjf <nicholsonjf@gmail.com>
2014-04-01 00:54:42 -0700

    Update reporting_structure.py

--- reporting_structure.py
+++ reporting_structure.py
@@ -1,22 +1,3 @@
-"""
-    At E*Trade, one of the features of the application I developed is to take data describing an
-    employee's access privileges to an application, and send it to the appropriate person in the
-    business for review and approval. It's not a terribly exciting problem to solve, but critical
-    for regulatory compliance.
-
-    There are different workflow rules, depending on the application. For instance, user access to App X
-    must be approved by the user's manager or above, whereas access to App Y need be reviewed by at least
-    a director (in the same reporting line as the user).
-
-    Thus, before routing any of the tens of thousands of records that come through the application each
-    quarter, I have to know who to route it to.
-
-    To solve this problem, I used a bit of recursion to create a nested dictionary and a function that returns
-    the reporting structure of any employee in that dict. Thus, no matter the workflow rule, I can access the id of
-    the right approver.
-
-"""
-
 import employees, sqlite3
 conn = sqlite3.connect('employees.db')
 cur = conn.cursor()
